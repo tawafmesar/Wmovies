9f3f476ec327cc8826f6432478f19f8f81d13a9d
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-08-06 15:07:31 +0400

    add Category popular movies and best week movies

--- app/src/main/java/com/example/wmovies/ui/MainActivity.java
+++ app/src/main/java/com/example/wmovies/ui/MainActivity.java
@@ -17,6 +17,7 @@ import com.example.wmovies.adapters.MovieItemClickListener;
 import com.example.wmovies.R;
 import com.example.wmovies.models.Slide;
 import com.example.wmovies.adapters.SliderPagerAdapter;
+import com.example.wmovies.utils.DataSource;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
     private List<Slide> lstSlides ;
     private ViewPager2 sliderpager;  // Use ViewPager2
     private TabLayout indicator;
-    private RecyclerView MoviesRV ;
+    private RecyclerView MoviesRV,moviesRvWeek ;
 
 
     @Override
@@ -40,11 +41,45 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
         setContentView(R.layout.activity_main);
 
 
-         sliderpager = findViewById(R.id.slider_pager);
-         indicator = findViewById(R.id.indicator);
-         MoviesRV = findViewById(R.id.Rv_movies);
+        iniViews();
+        iniSlider();
+        iniPopularMovies();
+        iniWeekMovies();
 
 
+
+    }
+
+    private void iniViews() {
+        sliderpager = findViewById(R.id.slider_pager);
+        indicator = findViewById(R.id.indicator);
+        MoviesRV = findViewById(R.id.Rv_movies);
+        moviesRvWeek = findViewById(R.id.rv_movies_week);
+
+    }
+
+    private void iniWeekMovies() {
+
+
+
+        MovieAdapter weekMovieAdapter = new MovieAdapter(this, DataSource.getWeekMovies(),this);
+        moviesRvWeek.setAdapter(weekMovieAdapter);
+        moviesRvWeek.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
+
+    private void iniPopularMovies() {
+
+
+
+        MovieAdapter movieAdapter = new MovieAdapter(this, DataSource.getPopularMovies(),this);
+        MoviesRV.setAdapter(movieAdapter);
+        MoviesRV.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
+
+
+
+
+    private void iniSlider() {
         lstSlides = new ArrayList<>() ;
         lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
         lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
@@ -56,29 +91,10 @@ public class MainActivity extends AppCompatActivity implements MovieItemClickLis
         sliderpager.setAdapter(adapter);
 
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new MainActivity.SliderTimer(),4000,6000);
+        timer.scheduleAtFixedRate(new SliderTimer(),4000,6000);
 
         new TabLayoutMediator(indicator, sliderpager, (tab, position) -> {
         }).attach();
-
-
-
-
-
-        List<Movie> lstMovies = new ArrayList<>();
-        lstMovies.add(new Movie("Moana",R.drawable.moana,R.drawable.spidercover));
-        lstMovies.add(new Movie("Black P",R.drawable.blackp,R.drawable.spidercover));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-        lstMovies.add(new Movie("The Martian",R.drawable.themartian));
-
-        MovieAdapter movieAdapter = new MovieAdapter(this,lstMovies,this);
-        MoviesRV.setAdapter(movieAdapter);
-        MoviesRV.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-
-
-
     }
 
     @Override
