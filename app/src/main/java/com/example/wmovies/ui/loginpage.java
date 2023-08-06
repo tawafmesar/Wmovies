c7a45e36c4e991c4db2a2216679070940830169e
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-08-06 20:14:07 +0400

    Finish login & signup UI

---
+++ app/src/main/java/com/example/wmovies/ui/loginpage.java
@@ -0,0 +1,16 @@
+package com.example.wmovies.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wmovies.R;
+
+public class loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+    }
+}
---
+++ app/src/main/java/com/example/wmovies/ui/signuppage.java
@@ -0,0 +1,16 @@
+package com.example.wmovies.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wmovies.R;
+
+public class signuppage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signuppage);
+    }
+}
